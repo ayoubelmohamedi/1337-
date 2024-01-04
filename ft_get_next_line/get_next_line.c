a478d92ae0e6ed57df37d30d4372cb5475774c43
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-01-04 20:51:21 +0100

    all

--- ft_get_next_line/get_next_line.c
+++ ft_get_next_line/get_next_line.c
@@ -1,6 +1,37 @@
 
+#include "get_next_line.h"
 
 char *get_next_line(int fd)
 {
-     
+    int static line;
+    char * buff;
+    int i;
+
+    i = 0; 
+    line = 0;
+    if (fd < 0)
+        return (NULL);
+    int tot = read(fd,buff,buff_size); 
+   //figure out how to use (read) to reach end of line. 
+    while (buff[i] != "\0")
+    {
+        
+    }
+    buff = malloc((buff_size + 1) * sizeof(char));
+    if (!buff)
+        return (NULL);
+    free(buff);
+
+    return(buff);
 }
+
+int main()
+{
+    int fd = open("text.txt",O_RDONLY);
+    char * str = get_next_line(fd);
+    printf("%s\n", str);
+    close(fd);
+
+    return (0);
+}
+
---
+++ ft_get_next_line/get_next_line.h
@@ -0,0 +1,14 @@
+
+#ifndef GET_NEXT_LINE
+#define GET_NEXT_LINE 
+
+#define buff_size 1000
+
+//tobe removed
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <unistd.h> 
+#include <fcntl.h> 
+
+#endif
