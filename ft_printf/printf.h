07c28473210467ee950f8179caede46e9e8e302d
ayoubelmohamedi <ayoubpro61@gmail.com>
2024-01-08 16:00:07 +0100

    add extra functions

--- ft_printf/printf.h
+++ ft_printf/printf.h
@@ -15,18 +15,20 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int	ft_printf(char *content, ...);
-int	ft_handle(char spec, va_list *args);
-int	ft_handle_nbr(int nbr);
-int	ft_handle_usign(unsigned int nbr);
-int	ft_count(int nbr);
-int	ft_handle_hex(int nbr, char x);
-int	ft_handle_char(int c);
-int	ft_hande_str(char *str);
-int	ft_handleptr(void *ptr);
-int	ft_handle_perc(int perc);
-void ft_printnbr(int nbr, int size);
-int	power(int base, int pow);
-int ft_strchr(char c);
+int		ft_printf(char *content, ...);
+int		ft_handle(char spec, va_list *args);
+int		ft_handle_nbr(int nbr);
+int		ft_handle_usign(unsigned int nbr);
+int		ft_count(int nbr);
+int		ft_handle_hex(int nbr, char x);
+int		ft_handle_char(int c);
+int		ft_hande_str(char *str);
+int		ft_handleptr(void *ptr);
+int		ft_handle_perc(int perc);
+void	ft_printnbr(int nbr, int size);
+int		power(int base, int pow);
+int		ft_strchr(char c);
+int		ft_allspaces(char *str);
+int		ft_shorten(char **str);
 
 #endif
