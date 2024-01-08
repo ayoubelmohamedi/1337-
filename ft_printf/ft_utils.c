851b7d1fc5d2447bcd53f0ead151a09652c10205
ayoubelmohamedi <ayoubpro61@gmail.com>
2024-01-08 15:58:31 +0100

    add utils.c

---
+++ ft_printf/ft_utils.c
@@ -0,0 +1,44 @@
+#include "printf.h"
+
+int	ft_strchr(char c)
+{
+	char	*str;
+
+	str = "diuxXcsp";
+	while (*str)
+	{
+		if (c == *str)
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+int	ft_allspaces(char *str)
+{
+	int	allspace;
+
+	allspace = 1;
+	while (*str != '\0')
+	{
+		if (*str != ' ')
+			allspace = 0;
+		str++;
+	}
+	return (allspace);
+}
+
+int	ft_shorten(char **str)
+{
+	int i;
+
+	i = 0;
+	if ((*str)[i] == ' ')
+		write(1, (*str), 1);
+	while ((*str)[i] == ' ' && (*str)[i] != '\0')
+		i++;
+	(*str) += (i--);
+	if (i > 0)
+		return (1);
+	return (0);
+}
