5134a3bcdaa2189b5728a036dee14255ee475afa
ayoubelmohamedi <ayoubpro61@gmail.com>
2024-01-24 18:33:35 +0100

    fix segfaults

--- ft_get_next_line/get_next_line.c
+++ ft_get_next_line/get_next_line.c
@@ -6,44 +6,54 @@ char *get_next_line(int fd)
     char            buffer[BUFFER_SIZE]; 
     static char     *remain; 
     char            *line;
-    int             i;
+    size_t           i;
     size_t          size; 
 
     remain = ft_strdup("");
     size = read(fd,buffer,BUFFER_SIZE);
-    size  = 0;
+    printf("size => %d\n",size);
+    
     while (size > 0)
     {
         i = 0;
         buffer[size] = '\0';
+        printf("size => %d\n",size);
+        printf("buffer content %s\n",buffer);
+
         remain = ft_strjoin(remain, buffer);
-        while (buffer[i] && (buffer[i] != '\n' || buffer[i] != '\0'))
-        {
+        printf("helo\n");
+        while (buffer[i] && (buffer[i] != '\n' && buffer[i] != '\0'))
             i++;
+        if (buffer[i] == '\n')
             break;
-        }
         size = read(fd,buffer,BUFFER_SIZE);
     }
-    line = ft_getline(remain, sizeof(buffer));
+    line = ft_getline(remain);
     if (!line)
         return (0);
     return (line);
 }
 
-char *ft_getline(char *remain, size_t buffersize)
+char *ft_getline(char *remain)
 {
     char *line;
     char *tmp;
-    int i;
+    size_t i;
+    size_t j;
 
     i = 0;
-    while (remain[i] != '\n' || remain[i] != '\0')
+    while (remain[i] != '\n' && remain[i] != '\0')
         i++;
+    printf("%d\n",i);
     line = ft_substr(remain, 0, i);
+    printf("%s\n",line);
     if (!line)
         return (0);
     tmp = remain;
-    remain = ft_substr(remain,i, buffersize);
+    j = i;
+    while (remain[j])
+        j++;
+    remain = ft_substr(remain,i, j);
     free(tmp);
     if (!remain)
     {
@@ -56,10 +66,19 @@ char *ft_getline(char *remain, size_t buffersize)
 
 int main()
 {
-    int fd = open("text.txt", O_RDONLY);
-    char * res = get_next_line(fd);
-    char * res2 = get_next_line(fd);
-    printf("%s",res);
-    printf("%s",res2);
+    // int fd = open("text.txt", O_RDONLY);
+    // char * res = get_next_line(fd);
+    // char * res2 = get_next_line(fd);
+    // printf("%s",res);
+    // printf("%s",res2);
+    char *line = "hello there \n it's me";
+    int i = 0;
+    while (line[i] != '\n' && line[i] != '\0')
+        i++;
+    printf("i = %d\n",i);
+    char *res= ft_substr(line,0, i);
+    printf("%s\n",res);
+    
     return (0);
+
 }
