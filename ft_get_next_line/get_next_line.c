abef17c842a73a1c8707b5536a3c9fcf785480eb
ayoubelmohamedi <ayoubpro61@gmail.com>
2024-01-24 17:09:00 +0100

    get_next_line

--- ft_get_next_line/get_next_line.c
+++ ft_get_next_line/get_next_line.c
@@ -3,35 +3,63 @@
 
 char *get_next_line(int fd)
 {
-    int static line;
-    char * buff;
+    char            buffer[BUFFER_SIZE]; 
+    static char     *remain; 
+    char            *line;
+    int             i;
+    size_t          size; 
+
+    remain = ft_strdup("");
+    size = read(fd,buffer,BUFFER_SIZE);
+    size  = 0;
+    while (size > 0)
+    {
+        i = 0;
+        buffer[size] = '\0';
+        remain = ft_strjoin(remain, buffer);
+        while (buffer[i] && (buffer[i] != '\n' || buffer[i] != '\0'))
+        {
+            i++;
+            break;
+        }
+        size = read(fd,buffer,BUFFER_SIZE);
+    }
+    line = ft_getline(remain, sizeof(buffer));
+    if (!line)
+        return (0);
+    return (line);
+}
+
+char *ft_getline(char *remain, size_t buffersize)
+{
+    char *line;
+    char *tmp;
     int i;
 
-    i = 0; 
-    line = 0;
-    if (fd < 0)
-        return (NULL);
-    int tot = read(fd,buff,buff_size); 
-   //figure out how to use (read) to reach end of line. 
-    while (buff[i] != "\0")
+    i = 0;
+    while (remain[i] != '\n' || remain[i] != '\0')
+        i++;
+    line = ft_substr(remain, 0, i);
+    if (!line)
+        return (0);
+    tmp = remain;
+    remain = ft_substr(remain,i, buffersize);
+    free(tmp);
+    if (!remain)
     {
-        
+        free(remain);
+        return (0);
     }
-    buff = malloc((buff_size + 1) * sizeof(char));
-    if (!buff)
-        return (NULL);
-    free(buff);
-
-    return(buff);
+     return (line);
 }
+
 
 int main()
 {
-    int fd = open("text.txt",O_RDONLY);
-    char * str = get_next_line(fd);
-    printf("%s\n", str);
-    close(fd);
-
+    int fd = open("text.txt", O_RDONLY);
+    char * res = get_next_line(fd);
+    char * res2 = get_next_line(fd);
+    printf("%s",res);
+    printf("%s",res2);
     return (0);
 }
-
--- ft_get_next_line/get_next_line.h
+++ ft_get_next_line/get_next_line.h
@@ -2,7 +2,7 @@
 #ifndef GET_NEXT_LINE
 #define GET_NEXT_LINE 
 
-#define buff_size 1000
+#define BUFFER_SIZE 512
 
 //tobe removed
 #include <stdio.h>
@@ -10,5 +10,16 @@
 #include <stdlib.h>
 #include <unistd.h> 
 #include <fcntl.h> 
+#include <stddef.h>
+
+char    *get_next_line(int fd);
+char    *ft_getline(char *remain, size_t buffersize);
+char    *ft_strjoin(char *s1,char *s2);
+size_t  ft_strlen(char *str);
+char	*ft_strdup(char *s);
+char	*ft_substr(char *s, size_t start, size_t len);
+size_t	ft_strlcpy(char *dst, char *src, size_t size);
+
+
 
 #endif
