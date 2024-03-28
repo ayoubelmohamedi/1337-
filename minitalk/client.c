0da7c17b1069636aa2af8eb2eb101c86696edeaf
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-03-28 02:13:42 +0000

    sending bits from client

--- minitalk/client.c
+++ minitalk/client.c
@@ -1,16 +1,34 @@
 
 #include "minitalk.h"
 
+void sendbits(int pid, char c)
+{
+    int i;
+
+    i = 0;
+    while (i < 8)
+    {
+        if ((c & (0x01 << i)) != 0)
+            kill(pid, SIGUSR1);
+        else
+            kill(pid, SIGUSR2);
+        i++;
+        usleep(200);
+    }
+}
+
 int main(int ac,char **argv)
 {
+    size_t i;
+
     if (ac != 3)
         return (1);
+    i = 0;
     int pid = ft_atoi((const char*) argv[1]);
-    if (kill(pid, SIGUSR1) == -1)
+    while (argv[2][i])
     {
-        perror("(kill)");
-        return (1);
+        sendbits(pid, argv[2][i++]);
     }
-    printf("signal sent");
+    sendbits(pid, '\n');
     return (0);
-2 }
+}
