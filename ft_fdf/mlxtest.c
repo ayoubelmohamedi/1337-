578d6af87e8855613a0424f4b63206201d5d43fb
ayoubelmohamedi <ael-moha@c3r3p2.1337.ma>
2024-05-01 15:52:09 +0100

    all

--- ft_fdf/mlxtest.c
+++ ft_fdf/mlxtest.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "mlx.h"
 
+#include <ft_fdf.h>
+
 typedef	struct s_data
 {
 	void	*img;
@@ -20,13 +22,59 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-int	main(void)
+int *ft_split_int(char *line)
+{
+	int *res;
+	char *split;
+	size_t i;
+
+	i = 0;
+	split = ft_split();
+	while (line[i])	
+	{
+
+	}
+
+
+	return (res);
+}
+
+char **ft_genMap(char *filename)
+{
+	char **map;
+	char *line;
+	int fd;
+
+	fd = open(filename,O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	line = get_next_line(fd);
+	while (line)
+	{
+		map[i] = ft_split_int(line);
+		line = get_next_line(fd);
+	}
+
+
+
+	return (map);
+
+}
+
+int	main(itn ac, char **av)
 {
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
 	t_data	img2;
-	
+	int **map;
+	int fd;
+
+	if (ac != 2)
+		return (1);
+	map = ft_genMap(av[1]);
+	if (!map)
+		return (1);
 	mlx = mlx_init();
 
 	mlx_win = mlx_new_window(mlx, 720, 720, "Hello world!");
