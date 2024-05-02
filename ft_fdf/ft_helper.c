211d8cf7b1f5560fa94915cb351315baeb6238ff
ayoubelmohamedi <ael-moha@c4r4p5.1337.ma>
2024-05-02 22:35:20 +0100

    all

---
+++ ft_fdf/ft_helper.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_helper.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 20:51:30 by ael-moha          #+#    #+#             */
+/*   Updated: 2024/05/02 20:51:50 by ael-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "ft_fdf.h"
+
+
+size_t ft_getrows(char *filename)
+{
+	size_t rows;
+	char *line;
+	char *tmp;
+	int fd;
+
+	rows = 1;
+	fd = open(filename, O_RDONLY);
+	line = get_next_line(fd);
+	while(line)
+	{
+		rows++;
+		tmp = line;
+		line = get_next_line(fd);
+		free(tmp);
+	}
+	close(fd);
+	return (rows);
+}
+
+size_t ft_getcols(char *filename)
+{
+	size_t cols;
+	char *line;
+	char **split;
+	int fd;
+
+	fd = open(filename, O_RDONLY);
+	line = get_next_line(fd);
+	split = ft_split(line, ' ');
+	free(line);
+	cols = 0;
+	while (split[cols])
+	{
+		cols++;
+		free(split[cols-1]);
+	}
+	free(split);
+	close(fd);
+	return (cols);
+}
+
+int *ft_split_int(const char *line, size_t col)
+{
+	int *res;
+	char **split;
+	size_t i;
+
+	i = 0;
+	split = ft_split((const char *) line, ' ');
+	res = malloc(sizeof(int) * col);
+	while (split[i])
+	{
+		res[i] = ft_atoi((const char*)split[i]);
+		// free(&split[i]);
+		i++;
+	}
+	free((void*)line);
+	// free(split);
+	return (res);
+}
+
--- ft_fdf/mlxtest.c
+++ ft_fdf/mlxtest.c
@@ -21,111 +21,94 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-void	mappirize(t_data *data, int rows, int cols, int color)
+void	mappirize(t_data *data, t_point *points, int color)
 {
 	char *dst;
 	// offset => int offset = (y * line_length + x * (bits_per_pixel / 8));
 	size_t i, j;
+	int right, down;
+	int	new_x, new_y;
 
+	right = 0;
+	down = 0;
 	i =0;
 	j =0;
-
 	while (i < rows - 1)
 	{
 		while (j < cols -1 )
 		{
-			
-			mlx_pixel_put(mlx, win, 640/2, 360/2, 0xFFFFFF);
+			new_x = points[i]->x + (sqrt(2) / 2) * points[i]->y + (sqrt(2) / 2) * points[i]->z ;
+			new_y = points[i]->x - (sqrt(2) / 2) * points[i]->y + (sqrt(2) / 2) * points[i]->z ;
+
+			my_mlx_pixel_put(data, new_x, new_y, 0xFFFFFF);
+			right += 4;
+			j++;
 		}
+		right = 0;
+		down += 4;
 		printf("\n");
 		j = 0;
 		i++;
 	}
-
-
 }
 
-size_t ft_getrows(char *filename)
+void	freeItems(void **item, size_t cols)
 {
-	size_t rows;
-	char *line;
-	char *tmp;
-	int fd;
-
-	rows = 1;
-	fd = open(filename, O_RDONLY);
-	line = get_next_line(fd);
-	while(line)
-	{
-		rows++;
-		tmp = line;
-		line = get_next_line(fd);
-		free(tmp);
-	}
-	close(fd);
-	return (rows);
-}
-
-size_t ft_getcols(char *filename)
-{
-	size_t cols;
-	char *line;
-	char **split;
-	int fd;
-
-	fd = open(filename, O_RDONLY);
-	line = get_next_line(fd);
-	split = ft_split(line, ' ');
-	free(line);
-	cols = 0;
-	while (split[cols])
-	{
-		cols++;
-		free(split[cols-1]);
-	}
-	free(split);
-	close(fd);
-	return (cols);
-}
-
-int *ft_split_int(const char *line, size_t col)
-{
-	int *res;
-	char **split;
 	size_t i;
 
 	i = 0;
-	split = ft_split((const char *) line, ' ');
-	res = malloc(sizeof(int) * col);
-	while (split[i])
-	{
-		res[i] = ft_atoi((const char*)split[i]);
-		// free(&split[i]);
-		i++;
-	}
-	free((void*)line);
-	// free(split);
-	return (res);
+	while (i < cols)
+		free(item[i++]);
+	free(item);
 }
 
-int **ft_genMap(char *filename, size_t rows, size_t cols)
+int		ft_fetchColor(char *text)
 {
-	int **map;
+	if (ft_strchr)
+
+
+}
+
+t_map	*ft_parseLine(char **splitted, size_t curr_row, size_t cols)
+{
+	t_map *row_map;
+	size_t i;
+
+	i = 0;
+	row_map = (t_map*)malloc(sizeof(t_map*) * (cols + 1));
+	while (splitted[i])
+	{
+		row_map[i]->x = i;
+		row_map[i]->y = curr_row;
+		row_map[i]->z = ft_atoi(splitted[i]);
+		row_map[i]->color = ft_fetchColor(splitted[i]) 
+		i++;
+	}
+	freeItems((void*)splitted, cols);
+	row_map[cols + 1] = NULL;
+	return (row_map);
+}
+
+t_point **ft_genMap(char *filename, size_t rows, size_t cols)
+{
+	t_point **map;
+	t_map	*row_map;
 	char *line;
 	int fd;
 	char *tmp;
 	size_t i;
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);	
 	if (fd < 0)
 		return (NULL);
+	map = malloc(sizeof(t_point*) * (rows + 1));
+	map[rows+1]= NULL;
 	line = get_next_line(fd);
-	map = malloc(sizeof(int*) * rows);
 	i = 0;
 	while (line)
 	{
 		tmp = line;
-		map[i++] = ft_split_int(line, cols);
+		map[i++] = ft_parseLine(ft_split(line, ' '), cols); 
 		line = get_next_line(fd);
 		// free(tmp);
 	}
@@ -137,8 +120,7 @@ int	main(int ac, char **av)
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
-	t_data	img2;
-	int **map;
+	t_point **map;
 
 	if (ac != 2)
 		return (1);
@@ -149,10 +131,8 @@ int	main(int ac, char **av)
 	int fd = open(av[1], O_RDONLY);
 	char *line = get_next_line(fd);
 	// printf("%s \n", line);
-	
 	map = ft_genMap(av[1], rows, cols);
 	close(fd);
-
 	printf("cols %zu\n", cols);	
 	printf("rows %zu\n", rows);	
 	mlx = mlx_init();
@@ -161,8 +141,7 @@ int	main(int ac, char **av)
 	img.img = mlx_new_image(mlx, 720, 720);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
-	
-	my_mlx_pixel_put(&img, rows,cols,0xffffff);
+	mappirize(&img, rows, cols, 0xFFFFFF);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
-
 }
