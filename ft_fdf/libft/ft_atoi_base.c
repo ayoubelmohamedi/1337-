c4ecf46237c75997b0b4cbc9e7a8944e24a810f4
Ayoub El mohamedi <ael-moha@c3r5p3.1337.ma>
2024-08-02 10:37:50 +0100

    fix atoi_base

--- ft_fdf/libft/ft_atoi_base.c
+++ ft_fdf/libft/ft_atoi_base.c
@@ -14,37 +14,12 @@
 #include "libft.h"
 
 
-static  int get_base_length(char *base)
-{
-	int	base_length;
-	int	j;
-
-	base_length = 0;
-	while (base[base_length])
-	{
-		if (base[base_length] == '-' || base[base_length] == '+')
-			return (0);
-		j = base_length + 1;
-		while (base[j])
-		{
-			if (base[base_length] == base[j])
-				return (0);
-			++j;
-		}
-		++base_length;
-	}
-	if (base_length < 2)
-		return (0);
-	return (base_length);
-}
-
 static  int	check_errors(char *str, char *base)
 {
 	int	i;
 	size_t b_len;
 
-	b_len = ft_strlen(base);
-	if (!base || b_len < 2)
+	if (!base || ((b_len = ft_strlen(base)) < 2))
 		return (1);	
 	i = 1;
 	while(base[i])
@@ -79,16 +54,15 @@ int	ft_atoi_base(char *str, char *base)
 	int	sign;
 	int	base_length;
 
+	res = 0;
+	i	= 0;
 	sign = 1;
 	if (*str == '+' || *str == '-')
 		if (*str++ == '-')
 			sign = -1;
-	if (*str == )
-	if (!(base_length = get_base_length(base)) || check_errors(str, base))
+	if (check_errors(str, base))
 		return (0);
-	res = 0;
-	sign = 1;
-	
+	base_length = ft_strlen(base);
 	while (str[i])
 		res = (res * base_length) + (get_nbr(str[i++], base));
 	return (res * sign);
