0a00cecde6445546906c6c93eeb382f4ccbd59f5
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-10-02 18:52:43 +0100

    moved functions to ft_routines.c for mandatory

---
+++ ft_philo/philo_mandatory/ft_routines.c
@@ -0,0 +1,47 @@
+
+
+#include "ft_philo.h"
+
+
+void ft_usleep(size_t time_to_sleep)
+{
+	size_t current = current_time_in_milliseconds();
+	while (current_time_in_milliseconds() - current < time_to_sleep)
+		usleep(500);
+}
+
+void ft_think(t_philo *philo) 
+{
+	LOCK(philo->all->output_mtx);
+	printf(AC_BLUE "%zu %d is thinking\n" RESET , current_time_in_milliseconds() - philo->all->curr_time, philo->index);
+	UNLOCK(philo->all->output_mtx);
+	ft_usleep((philo->all->t_die - (current_time_in_milliseconds() - philo->last_eat)) / 2);
+}
+
+void ft_eat(t_philo *philo)
+{
+	LOCK(philo->my_fork);
+	printf(AC_YELLOW "%zu %d has taken a fork\n" RESET, current_time_in_milliseconds() - philo->all->curr_time, philo->index);
+	LOCK(philo->r_fork);
+	printf(AC_YELLOW "%zu %d has taken a fork\n" RESET, current_time_in_milliseconds() - philo->all->curr_time, philo->index);
+
+	LOCK(philo->all->output_mtx);
+	printf(AC_RED "%zu %d is eating\n" RESET, current_time_in_milliseconds() - philo->all->curr_time, philo->index);
+	UNLOCK(philo->all->output_mtx);	
+	LOCK(philo->all->meal_mtx);
+	philo->last_eat = current_time_in_milliseconds();	
+	UNLOCK(philo->all->meal_mtx);
+
+	ft_usleep(philo->all->t_eat);
+	UNLOCK(philo->my_fork);
+	UNLOCK(philo->r_fork);
+}
+
+void ft_sleeping(t_philo *philo)
+{
+	LOCK(philo->all->output_mtx);
+	printf(AC_GREEN "%zu %d is sleeping\n" RESET, current_time_in_milliseconds() - philo->all->curr_time, philo->index);
+	UNLOCK(philo->all->output_mtx);
+	ft_usleep(philo->all->t_sleep);
+}
+
