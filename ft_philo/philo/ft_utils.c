0c7066a00f37827e782f3bb7ee4679c4d559e7a9
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-10-05 00:02:20 +0100

    done parsing madatory

--- ft_philo/philo/ft_utils.c
+++ ft_philo/philo/ft_utils.c
@@ -30,17 +30,17 @@ int	ft_is_nbr(char *nbr)
 
 int ft_check_max_zero(int ac, char **args)
 {
-	if (ac > 5 || ac <= 1)
-	{
-		ft_perror('c');
-		return (1);
-	}
-	if (ac > 4 && ft_is_nbr(args[1]) && ft_atoi(args[1]) == 0) 
+	if (ac != 5 && ac != 6)
+    {
+        ft_perror('c'); 
+        return (0);
+    }	
+	if (ft_is_nbr(args[1]) && ft_atoi(args[1]) == 0) 
 	{
 		ft_perror('z');
-		return (1);
+		return (0);
 	}
-	return (0);
+	return (1);
 }
 
 int	is_valid(int ac, char **args)
@@ -48,24 +48,18 @@ int	is_valid(int ac, char **args)
 	size_t i;
 
 	i = 1;
-	if (ft_check_max_zero(ac, args))
+	if (!ft_check_max_zero(ac, args))
 		return (0);
-	while (i < 4)
+	while (i < ac)
 	{
-		if (!ft_is_nbr(args[i]) || ft_atoi(args[i]) < 0)
 		{
-			ft_perror('p');
-			return (0);
+			if (!ft_is_nbr(args[i]))
+			{
+				ft_perror('p');
+				return (0);
+			}
 		}
 		i++;
-	}
-	if (ac == 5)
-	{
-		if (!ft_is_nbr(args[5]) && ft_atoi(args[5]) < 0)
-		{
-			ft_perror('p');
-			return (0);
-		}
 	}
 	return (1);
 }
