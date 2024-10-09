eb0c203ed050ff723b0ce14974ef0ecc97fcb769
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-10-09 17:43:54 +0100

    add get_current_time func in utils.c

--- ft_philo/philo_bonus/ft_routines.c
+++ ft_philo/philo_bonus/ft_routines.c
@@ -4,6 +4,13 @@
 #include "ft_philo_bonus.h"
 
 
+size_t current_time_in_milliseconds()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (((tv.tv_sec) * 1000) + ((tv.tv_usec) / 1000));
+}
+
 void ft_usleep(size_t time_to_sleep)
 {
 	size_t current = current_time_in_milliseconds();
