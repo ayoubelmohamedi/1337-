d6609730e83b94c95e7b1bd7caee4db20f7ce74e
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-10-09 21:08:42 +0100

    moved strlen to ft_gen_name.c

--- ft_philo/philo_bonus/ft_utils.c
+++ ft_philo/philo_bonus/ft_utils.c
@@ -2,18 +2,82 @@
 #include "ft_philo_bonus.h"
 
 
-size_t ft_strlen(char *name)
+int	ft_isdigit(int c)
 {
-    size_t i;
-
-    i = 0;
-    while (name[i])
-        i++;
-    return (i);
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }
 
-size_t current_time_in_milliseconds() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return (((tv.tv_sec) * 1000) + ((tv.tv_usec) / 1000));
+int	ft_is_nbr(char *nbr)
+{
+	size_t	i;
+
+	i = 0;
+	while (nbr[i])
+	{
+		if (!ft_isdigit(nbr[i++]))
+			return (0);
+	}
+	return (1);
+}
+
+int ft_check_max_zero(int ac, char **args)
+{
+	if (ac != 5 && ac != 6)
+    {
+        ft_perror('c'); 
+        return (0);
+    }	
+	if (ft_is_nbr(args[1]) && ft_atoi(args[1]) == 0) 
+	{
+		ft_perror('z');
+		return (0);
+	}
+	return (1);
+}
+
+int	is_valid(int ac, char **args)
+{
+	size_t i;
+
+	i = 1;
+	if (!ft_check_max_zero(ac, args))
+		return (0);
+	while (i < ac)
+	{
+		if (!ft_is_nbr(args[i]))
+		{
+			ft_perror('p');
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	ft_atoi(const char *nbr)
+{
+	int			sign;
+	int			i;
+	long long	res;
+
+	sign = 1;
+	res = 0;
+	i = 0;
+
+	if (nbr == NULL)
+		return (0);
+	while (nbr[i] == '\t' || nbr[i] == '\n' || nbr[i] == '\v' || nbr[i] == '\f'
+		|| nbr[i] == '\r' || nbr[i] == ' ')
+		i++;
+	if (nbr[i] == '-' || nbr[i] == '+')
+	{
+		if (nbr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (nbr[i] >= '0' && nbr[i] <= '9')
+		res = (res * 10) + (nbr[i++] - '0');
+	return (res * sign);
 }
